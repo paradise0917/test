d68021d9f66f39abcfb6c1bb4a1541fefcf34dc1
paradise0917 <paradise0917@gmail.com>
2022-01-10 17:40:12 +0800

    feat: add Home screen and route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { ScreenA } from './screen/ScreenA';
 import { ScreenB } from './screen/ScreenB';
+import { Home } from './screen/Home';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/screenA' element={<ScreenA />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/screenA' element={<ScreenA />} />
         <Route path='/screenB' element={<ScreenB />} />
       </Routes>
     </BrowserRouter>
---
+++ src/screen/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+      <h2 color="#fff">Home Title</h2>
+      <div>Home Content</div>
+    </>
+  );
+};
